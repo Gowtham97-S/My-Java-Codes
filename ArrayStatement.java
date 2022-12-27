3e07a1915dcc863edb715573ef151a5e503aa144
Gowtham <121371102+Gowtham97-S@users.noreply.github.com>
2022-12-27 21:31:08 +0530

    Update ArrayStatement.java

--- ArrayStatement.java
+++ ArrayStatement.java
@@ -7,16 +7,11 @@ public class ArrayStatement {
 	int A[] = {5,6,8,5};
 	
 	System.out.println(A[2]);
-	
-	
+		
 	int arys[]= new int[4];
 	 arys[0]=10;
 	 arys[1]=20;
 	System.out.println(arys[1]);
-	
-	
-	
-	
 	
 	}
 }
